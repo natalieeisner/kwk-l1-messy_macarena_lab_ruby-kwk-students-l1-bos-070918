38bfca2540c55ada07d7c82255ae33b58f9fb5a6
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-10 14:32:08 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -18,7 +18,7 @@ puts "Step 3:Turn your right palm up."
 puts "Step 4: Turn your left palm up."
 
 # Step 5: FIXME
-puts ""Step 5: ."Place your right hand on your left shoulder."
+puts Step 5: ."Place your right hand on your left shoulder."
 
 # Step 6: FIXME
 puts "Place your left hand on your right shoulder"
